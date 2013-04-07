15c55a72281f3c0efb83536b9eda092f406904bd
Jimmy Liang <lyh@yazotech.com>
2013-04-08 02:42:48 +0800

    add assets

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Ewtt
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['website/all.css', 'admin/admin.css', 'website/all.js', 'admin/admin.js', 'admin/init.js', 'admin/seat_table.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
