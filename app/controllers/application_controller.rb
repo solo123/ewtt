dbccb7a1c132e91996ae28d50a9735b84db135b2
Jimmy Liang <lyh@yazotech.com>
2013-04-08 23:00:45 +0800

    v1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  helper TravelWebsite::ApplicationHelper
 
   def layout_by_resource
-    if devise_controller?
-      if resource_name == :employee
-        "travel_admin/login"
-      else
-        "travel_website/login"
-      end
+    if devise_controller? && resource_name == :employee
+      "travel_admin/login"
     else
       "travel_website/application"
     end
