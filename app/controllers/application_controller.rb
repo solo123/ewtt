71b9bada95f8e163589dca0788fb77e7480aa3d5
Jimmy Liang <lyh@yazotech.com>
2013-09-27 00:44:07 +0800

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   helper TravelWebsite::ApplicationHelper
 
   def layout_by_resource
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
       "travel_website/application"
     end
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login_name, :email, :password) }
+      devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:login, :password, :remeber_me) }
+    end
 end
 
